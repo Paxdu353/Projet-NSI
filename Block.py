7c225b6c6d72b34a81bcc7cd580b2b0f20b88c59
Gr-umpy <165896066+Gr-umpy@users.noreply.github.com>
2024-04-05 11:50:34 +0200

    Add files via upload
    version 0.1

---
+++ Block.py
@@ -0,0 +1,24 @@
+import pygame
+
+class Block :
+
+    def __init__(self, x, y, sprite, crossable = True):
+        self.x = x
+        self.y = y
+        self.sprite = sprite
+        self.hitbox = self.sprite.rect
+        self.crossable = crossable
+
+    def draw(self, screen, scroll):
+        self.hitbox.x = self.x - scroll[0]
+        self.hitbox.y = self.y - scroll[1]
+        if 0 <= self.hitbox.x < screen.get_width() and 0 <= self.hitbox.y < screen.get_height() :
+            screen.blit(self.sprite.image, self.hitbox)        
+
+        
+pygame.init()
+
+LARGEUR_ECRAN, HAUTEUR_ECRAN = 500, 400
+TAILLE_ECRAN = (LARGEUR_ECRAN, HAUTEUR_ECRAN)
+
+screen = pygame.display.set_mode(TAILLE_ECRAN)
