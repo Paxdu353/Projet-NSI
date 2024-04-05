37a619167ab30124108d6c839bac7e6b64e899a8
Paxdu353 <98526225+Paxdu353@users.noreply.github.com>
2024-04-05 11:45:12 +0200

    Create Sprites.py

---
+++ Sprites.py
@@ -0,0 +1,44 @@
+import pygame
+import glob
+
+class Sprite(pygame.sprite.Sprite):
+    """
+    :param id: correspond au sprite dans le dictionnaire 'SPRITES'
+    :param img: correpond au chemin relatif de l'image
+    """
+
+    def __init__(self, id):
+        super().__init__()
+        try:
+            self.filename = glob.glob(f'sprites/*_Capa-{id}.png')[0]
+            self.image = pygame.image.load(self.filename).convert_alpha()
+        except IndexError:
+            raise IndexError('Le sprite n\'existe pas')
+
+        self.id = id
+        self.rect = self.image.get_rect()
+
+        # CONSTANTE
+        self.screen = pygame.display.get_surface()
+
+    def draw_debug(self, x, y):
+        """
+            :param x: x position de type int
+            :param y: y position de type int
+
+            Affiche le sprite sur la surface self.screen
+            :return: Nothing
+        """
+        self.screen.blit(self.image, (x, y))
+
+    def scale(self, factor):
+        """
+
+        :param factor: Facteur d'agrandissement de type int
+        Permet de redimmensionner le sprite
+        :return: Nothing
+        """
+        new_height = self.image.get_height() * factor
+        new_width = self.image.get_width() * factor
+
+        self.image = pygame.transform.scale(self.image, (new_width, new_height))
