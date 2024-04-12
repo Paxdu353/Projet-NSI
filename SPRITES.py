c8193fee36a30fdf8150d3fc7c0cef15f2c87362
Qceet <165080891+Qceet@users.noreply.github.com>
2024-04-12 10:24:03 +0200

    Create SPRITES.py
    mon ptit lulu

---
+++ SPRITES.py
@@ -0,0 +1,199 @@
+SPRITES={   #Dictionnaire sprites
+
+
+                            #SOL
+    "BRIDGE_SPRITES" : {
+        "left":107,
+        "center":108,
+        "right":109
+    },
+    
+    "CROPS_FARM" : {
+        "FARM1":110,
+        "FARM2":112,
+        "FARM3":113,
+        "FARM4":114,
+        "FARM5":115,
+        "FARM6":116,
+        "FARM7":117,
+    },
+
+    "GRASS":{
+        "Grass_Flower1":118,
+        "Grass_Flower2":218,
+    },
+
+
+    "ROAD":{
+        "Vertical_whiteTapeLeft_road":30,
+        "BigCorner_topRight_road":31,
+        "BigCorner_bottomRight_road":32,
+        "BigCorner_bottomLeft_road":33,
+        "BigCorner_topLeft_road":34,
+        "mid_whiteTape_road":35,    #whitetape = ligne blanche
+        "MediumCorner_topRight_road":36,
+        "MediumCorner_bottomRight_road":37,
+        "MediumCorner_bottomLeft_road":38,
+        "MediumCorner_topLeft_road":39,
+        "Vertical_whiteTapeRight_road":40,
+        "SmallCorner_topRight_road":41,
+        "SmallCorner_bottomRight_road":42,
+        "SmallCorner_bottomLeft_road":43,
+        "SmallCorner_topLeft_road":44,
+        "Horizontal_whiteTapeTop_road":45,
+        "Horizontal_whiteTapeCenter_road":46,
+        "Horizontal_whiteTapebottom_road":47,
+        "Vertical_PedestrianCrossingLeft_road":48,
+        "Vertical_PedestrianCrossingCenter_road":49,
+        "Vertical_PedestrianCrossingRight_road":50,
+        "Honrizontal_PedestrianCrossingTop_road":51,
+        "Horizontal_PedestrianCrossingCenter_road":52,
+        "Horizontal_PedestrianCrossingBottom_road":53,
+        "All_Road":54,
+        "All_Road_ManholeCover":55, #Plaque d'égout
+    },
+
+    "GRASSPATH":{
+        "CenterDot_Path":66,
+        "Cross_Path":67,
+        "Horizontal_line_Path":68,
+        "Vertical_line_Path":69,
+        "Vertical_line_Path_+_Horizontal_LeftLine_Path":70,
+        "TopToLeft_Path":71,
+        "LeftToBottom_Path":72,
+        "Horizontal_line_Path_+_Vertical_BottomLine_Path":73,
+        "TopToRight_Path":74,
+        "RightToBottom_Path":75,
+        "Vertical_line_Path_+_Horizontal_RightLine_Path":76,
+        "Horizontal_line_Path_+_Vertical_TopLine_Path":77,
+    },
+
+                            #ANIMATION
+    "BIG_ZOMBIE_ANIMATION" : {
+        "face":413,
+        "faceLeft":[414,416,417],
+        "faceRight":415,
+        "back":418,
+        "back2":419,
+        "back3":420
+    },
+
+    "BIG_ZOMBIE_DAMAGE_ANIMATION" : {
+        "face_damaged_zombie": [422, 423, 424],
+        "left_damaged_zombie": [425,426,427],
+        "back_damaged_zombie": [428,429,430],
+
+    },
+
+    "BLOOD_ANIMATION" : {
+        "blood1":[380,381,382,383,384]
+    },
+
+
+    "DEAD_ZOMBIE_ANIMATION":{
+        "Floor_Zombie": [291,294,296],
+        "StandAgainstWall_Zombie" : [292,293],
+    },
+
+    "EXPLODING_BARREL_ANIMATION":{
+        "EXPLODING_BARREL":[147,148,149,150],
+    },
+
+    "EXPLOSION_ANIMATION":{
+        "EXPLOSION" : [359,360,361,362,363,364]
+    },
+
+    "MUSICNOTE_ANIMATION":{
+        "MusicNote": [390,391,392],
+    },
+
+                        #PLAYER
+
+    "PLAYER_WALK_ANIMATION":{
+        "Player_Face_Walk":[477,478,479],
+        "Player_ToRith_Walk":[480,481,482],
+        "Player_Back_Walk":[483,484,485],
+    },
+
+
+
+
+                        #DECORATIONS
+
+
+    "DRIVABLE_CAR": {
+        "CarNose_Down":165,
+        "CarNose_BottomLeft":166,
+        "CarNose_Left":167,
+        "CarNose_TopLeft":168,
+        "CarNose_Top":169,
+        "CarNose_TopRight":170,
+        "CarNose_Right":171,
+        "CarNose_BottomRight":172,
+        
+    },                   
+    "BROKEN_CARS" : {
+        "TopLeft_RedCar_WW":180,
+        "TopLeft_RedCar_FT":181,
+        "TopLeft_RedCar_BS":182,
+        "Top_RedCar_BS":183,
+        "TopLeft_GreyCar_WW":184,
+        "TopLeft_GreyCar_FT":185,
+        "TopLeft_GreyCar_BS":186,
+        "Top_GreyCar_BS":187,
+        "TopLeft_BeigeCar_WW":188,
+        "TopLeft_BeigeCar_FT":189,
+        "TopLeft_BeigeCar_BS":190,
+        "Top_BeigeCar_BS":191,
+    },
+
+    "BROKEN_TIRES" : {
+       "Top_ThreeTires":192,
+       "Top_OneTire":193,
+       "Top_TwoTires":194,
+    },
+
+    "BUSHES":{
+        "BottomLeft":82,
+        "Left":83,
+        "topLeftCorner":84,
+        "Top":85,
+        "Top+Background":86,
+        "SoloBush":87,
+        "BottomRight":88,
+        "Right":89,
+        "TopRightCorner":90,
+        "SoloBush2":91,
+    },
+
+    "FENCES":{
+        "TopView_TopLeft":127,
+        "TopView_TopRight":128,
+        "TopView_BottomLeft":129,
+        "TopView_BottomRight":130,
+        "TopView_Vertical":131,
+        "TopView_Vertical2":132,
+        "TopView_HorizontalLeft":133,
+        "TopView_HorizontalRight":134,
+    },  
+
+    "STRAW":{
+        "Center_Straw":119,
+        "TopLeft_Starw":120,
+        "BottomLeft_Straw":121,
+        "TopRight_Straw":122,
+        "BottomRight":123,
+        "TwoStraw_EmptyMid":124,
+    },
+
+
+                            #BATIMENTS
+    "GAS_STATION":{
+        "Station":226,
+        "Pomp_bottom":144,
+        "Pomp_top":145
+    },
+}
+
+                            #OBJET
+        
