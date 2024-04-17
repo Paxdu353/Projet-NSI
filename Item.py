846a437c8203789b0a6258702e336b689317cd10
Gr-umpy <165896066+Gr-umpy@users.noreply.github.com>
2024-04-17 10:39:54 +0200

    Update Item.py

--- Item.py
+++ Item.py
@@ -1,52 +1,55 @@
+from fileinput import filename
+from tarfile import BLOCKSIZE
 import Block, Sprite, pygame
 
 class Item :
 
-    def __init__(self) :
+    def __init__(self, filename = None) :
+        """ Création d'un item
         
+        `filename` : si renseigné, charge l'item depuis un fichier """
+
         self.x = 0
         self.y = 0
         self.blocks = []
 
-    def add (self, x, y, id_sprite):
+        if filename != None:
+            with open(filename, "r") as fichier:
+                contenu = fichier.readlines()
+                print(contenu)
+                for block in contenu:
+                    block = block.split(",")
+                    block[2] = block[2][:-1]
+                    self.add(int(block[0]), int(block[1]), int(block[2]))
+                    print(block)
+
+                
+
+    def add(self, x, y, id_sprite, size = Block.DEFAULT_SIZE):
+        """ Ajoute un bloc à la liste
+
+        x,y : coordonnées relatives du bloc
+        id_sprite : id du sprite choisi pour le bloc
+         """
         sprite = Sprite.Sprite(id_sprite)
-        block = Block.Block(x, y, sprite)
+        block = Block.Block(x / size, y / size, sprite, size)
         self.blocks.append(block)
 
     def draw(self, screen, scroll):
         for block in self.blocks:
             block.draw(screen , scroll)
     
-    def draw_debug(self, screen : pygame.Surface, zoom_factor):
-        self.zoom(zoom_factor)
+    def draw_debug(self, screen : pygame.Surface, size):
         for block in self.blocks:
+            block.resize(size)
             block.draw(screen , (0,0))
         
         # Dessin des lignes verticales
-        for x in range(16*zoom_factor, screen.get_width(), 16*zoom_factor):
+        for x in range(size, screen.get_width(), size):
             pygame.draw.line(screen, (255,0,0), (x, 0), (x, screen.get_height()))
         # Dessin des lignes horizontale
-        for y in range(16*zoom_factor, screen.get_height(), 16*zoom_factor):
+        for y in range(size, screen.get_height(), size):
             pygame.draw.line(screen, (255,0,0), (0, y), (screen.get_width(), y))
-
-    def move(self, x, y):
-        self.x = x
-        self.y = y
-        
-        for block in self.blocks:
-            block.x += x
-            block.y += y
-    
-    def zoom(self, factor):
-        for block in self.blocks:
-            block.sprite.scale(factor)
-            block.x *= factor
-            block.y *= factor
-
-"""
-    
-    def load(self, filename):
     
     def save(self, filename):
-        
-"""
+        pass
