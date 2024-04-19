acbbc19b84fd5e39ca88c167e8ddf7cf2825d1ee
Gr-umpy <165896066+Gr-umpy@users.noreply.github.com>
2024-04-19 11:13:21 +0200

    Update Item.py

--- Item.py
+++ Item.py
@@ -10,20 +10,20 @@ class Item :
         self.x = 0
         self.y = 0
         self.blocks = []
-
+        
         if filename != None:
             with open(filename, "r") as fichier:
                 contenu = fichier.readlines()
                 print(contenu)
                 for block in contenu:
                     block = block.split(",")
-                    block[2] = block[2][:-1]
-                    self.add(int(block[0]), int(block[1]), int(block[2]))
+                    block[3] = block[3][:-1]
+                    self.add(int(block[0]), int(block[1]), int(block[2]), int(block[3]))
                     print(block)
 
                 
 
-    def add(self, x, y, id_sprite, size = Block.DEFAULT_SIZE):
+    def add(self, x, y, id_sprite, crossable = False, size = Block.DEFAULT_SIZE):
         """ Ajoute un bloc à la liste
 
         x,y : coordonnées relatives du bloc
@@ -31,8 +31,12 @@ class Item :
          """
         
         sprite = Sprite.Sprite(id_sprite)
-
-        block = Block.Block(x / size, y / size, sprite, size)
+        if crossable == 0:
+            block = Block.Block(x / size, y / size, sprite, size)
+        if crossable == 1:
+            block = Block.Block(x / size, y / size, sprite, size, True)
+        else:
+            block = Block.Block(x / size, y / size, sprite, size, crossable)
         self.blocks.append(block)
 
     def draw(self, screen, scroll):
@@ -43,6 +47,15 @@ class Item :
         for block in self.blocks:
             block.resize(size)
             block.draw(screen , (0,0))
+
+            if block.crossable:
+                surface = pygame.Surface(block.hitbox.size, pygame.SRCALPHA)
+                pygame.draw.rect(surface, (255,0,0,127), surface.get_rect())
+                screen.blit(surface, block.hitbox)
+            else:
+                surface = pygame.Surface(block.hitbox.size, pygame.SRCALPHA)
+                pygame.draw.rect(surface, (0,255,0,127), surface.get_rect())
+                screen.blit(surface, block.hitbox)
         
         # Dessin des lignes verticales
         for x in range(size, screen.get_width(), size):
@@ -54,4 +67,7 @@ class Item :
     def save(self, filename):
         with open(filename, "w") as fichier:
             for block in self.blocks:
-                fichier.write(f"{int(block.x*16)},{int(block.y*16)},{block.sprite.id}\n")
+                c = 0
+                if block.crossable:
+                    c = 1
+                fichier.write(f"{int(block.x*16)},{int(block.y*16)},{block.sprite.id},{c}\n")
