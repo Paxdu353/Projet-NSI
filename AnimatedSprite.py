34fd856a0b6eba2c3a2122139376f22633116848
Gr-umpy <165896066+Gr-umpy@users.noreply.github.com>
2024-04-24 13:51:21 +0200

    Update AnimatedSprite.py

--- AnimatedSprite.py
+++ AnimatedSprite.py
@@ -13,6 +13,13 @@ class AnimatedSprite():
 
         self.animation_time = animation_time
 
+        self.sprite = self.sprite_list[0]
+        self.rect = self.sprite.rect
+        self.image = self.sprite.image
+
+    def __repr__(self):
+        return str(self.sprite)
+
     def draw_debug(self, x, y):
         # Affiche le sprite aux coordonnées x,y
         self.sprite_list[self.index].draw_debug(x,y)
@@ -21,15 +28,17 @@ class AnimatedSprite():
         # Scale de tous les sprites de la liste
         for sprite in self.sprite_list:
             sprite.scale(factor)
-
-
+        
+        self.sprite = self.sprite_list[0]
+        self.rect = self.sprite.rect
+        self.image = self.sprite.image
 
     def update(self):
         current_time = pygame.time.get_ticks()
-        print(current_time)
         if current_time - self.last_update_time > self.animation_time:
             self.index = (self.index + 1) % len(self.sprite_list)
             self.last_update_time = current_time
 
-        print(self.index)
-
+        self.sprite = self.sprite_list[self.index]
+        self.rect = self.sprite.rect
+        self.image = self.sprite.image
